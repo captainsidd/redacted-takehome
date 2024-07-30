f423673a3e4ce7b1e4b7470a7e925c9f5d5848ce
Sid Nutulapati <sidgonuts@gmail.com>
2024-07-30 14:39:07 -0400

    update names

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 from flask import Flask
-from klarna import Klarna
+from server import Server
 
 # initialize Flask app
 app = Flask(__name__)
 
 # initialize Application
-klarna = Klarna()
+server = Server()
 
 @app.route('/')
 def hello_world():
-    return 'Klarna App Running!'
+    return 'Compute App Running!'
 
 @app.route('/fibonacci/<int:nth_number>')
 def fibonacci_handler(nth_number: int) -> str:
-  return klarna.fibonacci_handler(n=nth_number)
+  return server.fibonacci_handler(n=nth_number)
 
 @app.route('/ackermann/<int:m_number>/<int:n_number>')
 def ackermann_handler(m_number: int, n_number:int) -> int:
-  return klarna.ackermann_handler(m=m_number, n=n_number)
+  return server.ackermann_handler(m=m_number, n=n_number)
 
 @app.route('/factorial/<int:factorial_nth>')
 def factorial_handler(factorial_nth: int):
-  return klarna.factorial_handler(n=factorial_nth)
+  return server.factorial_handler(n=factorial_nth)
 
 @app.route('/_metrics/')
 def metrics_handler():
-  return klarna.get_metrics()
-
-
+  return server.get_metrics()
